117432caaf406f99548d849e93419e84b5a9d1f3
amoik <moik.andreas@gmail.com>
2017-02-07 23:47:59 +0100

    link cleanups

--- car.sh
+++ car.sh
@@ -2,7 +2,7 @@ compiled=1
 if [[ $1 == "format" || $1 == "fmt" ]]; then
 	clang-format -i main.cc include/* src/*
 else
-	clear && g++ ./*.cc ./src/*.cc -o program -I include -I /usr/include/drm -I . -lwayland-client -lm -lvulkan -lxcb -ldrm -lgbm -lpng -lglfw -lstbi
+	clear && g++ ./*.cc ./src/*.cc -o program -I include -I /usr/include/drm -I . -lvulkan -lglfw -lstbi
 	compiled=$?
 fi
 if [[ $1 != "co" && $compiled -eq 0 ]]; then
